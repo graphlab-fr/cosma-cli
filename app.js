767648e724289b8f10e5cba681961c18c3c56015
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-11-10 18:37:10 +0100

    Revert "[add] force atlas"
    This reverts commit ab169837458d30363b2e3ed68c17eedd485ce81e.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const fs = require('fs')
-    , Graph = require('graphology')
-    , forceAtlas2 = require('graphology-layout-forceatlas2')
     , yamlFrontmatter = require('yaml-front-matter')
     , yamlReader = require('js-yaml')
     , path = require('path')
@@ -10,7 +8,6 @@ const fs = require('fs')
     , config = yamlReader.safeLoad(fs.readFileSync('config.yml', 'utf8'));
 
 let id = 1
-    , graph = new Graph()
     , entities = { nodes: [], edges: [] }
     , files = fs.readdirSync(config.files_origin, 'utf8')
         .filter(file_name => path.extname(file_name) === '.md')
@@ -30,14 +27,7 @@ let id = 1
 
 for (let file of files) {
     let size = edges.getRank(files.map(file => file.links).flat(), file.metas.id, file.links.length);
-    entities.nodes.push({id: file.metas.id, label: file.metas.title, size: size * 10, x: rand.randFloat(40, 50), y: rand.randFloat(40, 50)});
-
-    graph.addNode(file.metas.id, {
-        label: file.metas.title,
-        size: size * 10,
-        x: rand.randFloat(10, 20),
-        y: rand.randFloat(10, 20)
-    });
+    entities.nodes.push(jsonify.node(file.metas.id, file.metas.title, size * 10, rand.randFloat(40, 50), rand.randFloat(40, 50)));
 
     if (file.links.length !== 0) {
         for (let link of file.links) {
@@ -46,32 +36,8 @@ for (let file of files) {
     }
 }
 
-const cardinalNodes = forceAtlas2(graph, {
-    iterations: 50,
-    settings: {
-      gravity: 10
-    }
-});
-
-let ix = 0;
-for (let prop in cardinalNodes) {
-    // console.log(`${property}: ${cardinalNodes[property]}`);
-    let x = cardinalNodes[prop]['x']
-        , y = cardinalNodes[prop]['y']
-
-    console.log(entities.nodes[ix]);
-
-    entities.nodes[ix] = jsonify.node(entities.nodes[ix].id, entities.nodes[ix].label, entities.nodes[ix].size, x, y)
-
-    ix++;
-}
-
-
-
 if (fs.existsSync('./data') === false) {
     fs.mkdirSync('./data') }
-
-    console.log(entities.nodes);
 
 fs.writeFile('./data/nodes.json', '[' + entities.nodes.join(',') + ']', (err) => {
     if (err) { return console.error( 'Err. write html file : ' + err) }
