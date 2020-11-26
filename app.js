fe54f9f6499d70039f72d090683c8bda1e25ff68
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-11-26 13:27:14 +0100

    [add] no target links filter

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fs = require('fs')
     , config = yamlReader.safeLoad(fs.readFileSync('config.yml', 'utf8'));
 
 let id = 1
+    , fileIds = []
     , entities = { nodes: [], edges: [] }
     , files = fs.readdirSync(config.files_origin, 'utf8')
         .filter(file_name => path.extname(file_name) === '.md')
@@ -33,13 +34,26 @@ let id = 1
                 console.log('File ' + file.metas.title + ' throw out : no title');
                 return false; }
 
-            for (let link of file.links) {
-                if (isNaN(link) === true) {
-                    console.log('File "' + file.metas.title + '" throw out : no valid link');
-                    return false; }
-            }
+            fileIds.push(file.metas.id);
 
             return file;
+        })
+        .map(function(file) {
+            let validLinks = [];
+
+            for (let link of file.links) {
+                if (fileIds.indexOf(Number(link)) !== -1 && isNaN(link) === false) {
+                    validLinks.push(link);
+                } else {
+                    console.log('Link "' + link + '" removed from file "' + file.metas.title + '" : no valid target');
+                }
+            }
+
+            return {
+                content: file.content,
+                metas: file.metas,
+                links: validLinks
+            };
         })
 
 const ids = files.map(file => file.links).flat();
