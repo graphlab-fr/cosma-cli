6d436ead8857c71dedd1a10bec5d8743bc28e022
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-12-02 10:54:43 +0100

    [add] record id & debug record

--- functions/autorecord.js
+++ functions/autorecord.js
@@ -1,10 +1,21 @@
 const fs = require('fs')
+    , moment = require('moment')
     , config = require('../app').config;
 
 function genMdFile(title, category) {
+
+    if (category === '') {
+        category = 'undefined'; }
+
+    if (category !== 'undefined' && config.categories.indexOf(category) === -1) {
+        console.log('Unknown category. Add it to config.yml beforehand. No record was created.');
+        return;
+    }
+
     const content =
 `---
 title: ${title}
+id: ${moment().format('YYYYMMDDhmmss')}
 category: ${category}
 ---
 
--- functions/record.js
+++ functions/record.js
@@ -7,29 +7,15 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 (async () => {
     let metas = {};
     
-    try {
-        metas.title = await new Promise((resolve, reject) => {
-            rl.question('title ? ', (answer) => { resolve(answer) })
-        })
+    metas.title = await new Promise((resolve, reject) => {
+        rl.question('title ? ', (answer) => { resolve(answer) })
+    })
 
-        metas.category = await new Promise((resolve, reject) => {
-            rl.question('category (default = undefined) ? ', (answer) => {
-                if (answer === '') {
-                    resolve('undefined'); }
+    metas.category = await new Promise((resolve, reject) => {
+        rl.question('category (default = undefined) ? ', (answer) => { resolve(answer) })
+    })
 
-                if (config.categories.indexOf(answer) === -1) {
-                    reject('Unknown category. Add it to config.yml beforehand. No record was created.'); }
-
-                resolve(answer);
-            })
-        })
-
-        createRecord(metas.title, metas.category);
-    } catch(error) {
-        console.log(error);
-    }
+    require('./autorecord').genMdFile(metas.title, metas.category);
     
     rl.close()
 })()
-
-require('./autorecord').genMdFile(title, category);
