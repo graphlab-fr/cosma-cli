3d23a79efb560643a7628a3306fa94078a4e0cd8
arthurperret <arthurperret@me.com>
2020-12-03 14:03:53 +0100

    [edit] Mauvais nom
    index -> cosmographe

--- functions/template.js
+++ functions/template.js
@@ -26,7 +26,7 @@ initializeSimulation();`;
 
         fs.writeFile('cosmographe.html', htmlRender, (err) => {
             if (err) { console.error( 'Err. write home index file: ' + err) }
-            console.log('create index.html file');
+            console.log('create cosmographe.html file');
         });
     });
 }
