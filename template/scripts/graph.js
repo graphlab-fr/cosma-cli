e0d377d40890c5e0c1fd408142d6e3558e18217a
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2021-05-20 11:26:49 +0200

    [edit] hover node group

--- template/scripts/graph.js
+++ template/scripts/graph.js
@@ -120,25 +120,38 @@ function initializeDisplay() {
                     d.fy = null; })
         )
         .on('mouseover', function(nodeMetas) {
-            const nodesToModif = node.filter(function(node) {
-                if (node.id === nodeMetas.id) {
-                    return false;
-                }
-                return true;
-            })
+            let nodesIdsHovered = [nodeMetas.id];
+
             const linksToModif = link.filter(function(link) {
                 if (link.source.id === nodeMetas.id || link.target.id === nodeMetas.id) {
+                    nodesIdsHovered.push(link.source.id, link.target.id);
                     return false;
                 }
                 return true;
             })
 
-            nodesToModif.style('opacity', '0.5');
-            linksToModif.style('stroke-opacity', '0.2');
+            const nodesToModif = node.filter(function(node) {
+                if (nodesIdsHovered.includes(node.id)) {
+                    return false;
+                }
+                return true;
+            })
+
+            const nodesHovered = node.filter(function(node) {
+                if (!nodesIdsHovered.includes(node.id)) {
+                    return false;
+                }
+                return true;
+            })
+
+            nodesHovered.classed('hover', true);
+            nodesToModif.classed('translucent', true);
+            linksToModif.classed('translucent', true);
         })
         .on('mouseout', function() {
-            node.style('opacity', 1)
-            link.style('stroke-opacity', 1);
+            node.classed('hover', false);
+            node.classed('translucent', false);
+            link.classed('translucent', false);
         })
 
     labels = node.append("text")
