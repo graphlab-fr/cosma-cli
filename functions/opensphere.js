820ea26110b5a71f5b77bf9f4253d60442c8b05d
Myllaume <guillaume.brioudes@myllaume.fr>
2022-08-10 15:53:40 +0200

    edit opensphere namespace

--- functions/opensphere.js
+++ functions/opensphere.js
@@ -10,20 +10,20 @@ const fs = require('fs')
 
 const Opensphere = require('../core/models/opensphere');
 
-module.exports = function(nodesFilePath, linksFilePath) {
-    if (fs.existsSync(nodesFilePath) === false) {
-        return console.error('\x1b[31m', 'Err.', '\x1b[0m', 'CSV file (nodes data) does not exist.'); }
+module.exports = function(recordsFilePath, linksFilePath) {
+    if (fs.existsSync(recordsFilePath) === false) {
+        return console.error('\x1b[31m', 'Err.', '\x1b[0m', 'CSV file (records data) does not exist.'); }
     if (fs.existsSync(linksFilePath) === false) {
         return console.error('\x1b[31m', 'Err.', '\x1b[0m', 'CSV file (links data) does not exist.'); }
 
-    const nodesFileContent = fs.readFileSync(nodesFilePath, 'utf-8');
-    const nodesData = parse(nodesFileContent, { columns: true, skip_empty_lines: true });
+    const recordsFileContent = fs.readFileSync(recordsFilePath, 'utf-8');
+    const recordsData = parse(recordsFileContent, { columns: true, skip_empty_lines: true });
 
     const linksFileContent = fs.readFileSync(linksFilePath, 'utf-8');
     const linksData = parse(linksFileContent, { columns: true, skip_empty_lines: true });
     
-    const nodes = Opensphere.formatArrayNodes(nodesData);
+    const records = Opensphere.formatArrayRecords(recordsData);
     const links = Opensphere.formatArrayLinks(linksData);
-    const opensphere = new Opensphere(nodes, links);
+    const opensphere = new Opensphere(records, links);
     // console.log(opensphere.config);
 }
