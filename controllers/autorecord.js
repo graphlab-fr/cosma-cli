064c7c9a8f13bf8128a423624528cc657ec222ab
Myllaume <guillaume.brioudes@myllaume.fr>
2022-09-13 13:05:44 +0200

    edit record save

--- controllers/autorecord.js
+++ controllers/autorecord.js
@@ -4,40 +4,43 @@
  * @copyright GNU GPL 3.0 ANR HyperOtlet
  */
 
+const readline = require('readline');
+
 const Record = require('../core/models/record');
 
 module.exports = function (title = '', type = 'undefined', tags = '') {
     const record = new Record(undefined, title, type, tags);
 
-    const result = record.saveAsFile();
-
-    switch (result) {
-        case true:
-            console.log('\x1b[32m', 'record saved', '\x1b[0m', `: ${record.title}.md`);
-            break;
-
-        case false:
-            console.error('\x1b[31m', 'Err.', '\x1b[0m', record.writeReport());
-            break;
-
-        case 'overwriting':
-            const readline = require('readline')
-                , rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-
-            (async () => {
-                new Promise((resolve, reject) => {
-                    rl.question(`Do you want to overwrite '${record.title}.md' ? (y/n) `, (answer) => {
-                        if (answer === 'y') { record.saveAsFile(true); }
+    record.saveAsFile()
+        .then(() => {
+            logRecordIsSaved();
+        })
+        .catch((err) => {
+            const { message, type } = err;
+            switch (type) {
+                case 'overwriting':
+                    rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+                    rl.question(`Do you want to overwrite '${record.title}.md' ? (y/n) `, async (answer) => {
+                        if (answer === 'y') {
+                            try {
+                                await record.saveAsFile(true);
+                                logRecordIsSaved();
+                            } catch (err) {
+                                console.error('\x1b[31m', 'Err.', '\x1b[0m', err.message);
+                            }
+                        }
                         rl.close();
                     })
-                })
-            })();
+                    return;
+                case 'fs error':
+                case 'report':
+                default:
+                    console.error('\x1b[31m', 'Err.', '\x1b[0m', message);
+                    return;
+            }
+        });
 
-            break;
-
-        default:
-            console.error('\x1b[31m', 'Err.', '\x1b[0m', result.join(' '));
-            break;
+    function logRecordIsSaved() {
+        console.log('\x1b[32m', 'record saved', '\x1b[0m', `: ${record.title}.md`);
     }
-
 }
--- controllers/batch.js
+++ controllers/batch.js
@@ -44,12 +44,12 @@ module.exports = function (filePath) {
         }
 
         const index = Cosmoscope.getIndexToMassSave();
-        const result = Record.massSave(data, index);
-
-        if (result === true) {
-            return console.log('\x1b[34m', 'Records generated', '\x1b[0m', `(${data.length})`)
-        } else {
-            return console.error('\x1b[31m', 'Err.', '\x1b[0m', 'Data file invalid items : ', result.join(','));
-        }
+        Record.massSave(data, index)
+            .then(() => {
+                return console.log('\x1b[34m', 'Records generated', '\x1b[0m', `(${data.length})`)
+            })
+            .catch((err) => {
+                return console.error('\x1b[31m', 'Err.', '\x1b[0m', err);
+            });
     })
 }
