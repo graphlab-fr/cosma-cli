bda879f53313635fe29284ca99653c829b974be1
Myllaume <guillaume.brioudes@myllaume.fr>
2022-09-22 13:59:26 +0200

    del moment dependencie

--- controllers/time.js
+++ controllers/time.js
@@ -4,7 +4,13 @@
  * @copyright GNU GPL 3.0 ANR HyperOtlet
  */
 
-const moment = require('moment')
-    , time = moment().format('YYYY-MM-DD_HH-mm-ss');
+const currentDate = new Date();
+const year = currentDate.getFullYear().toString().padStart(4, "0");;
+const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+const day = currentDate.getDate().toString().padStart(2, "0");
+const hour = currentDate.getHours().toString().padStart(2, "0");
+const minute = currentDate.getMinutes().toString().padStart(2, "0");
+const second = currentDate.getSeconds().toString().padStart(2, "0");
 
+const time = `${year}-${month}-${day}_${hour}-${minute}-${second}`;
 module.exports = time;
