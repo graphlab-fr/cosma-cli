e1ce0bf284360d3ee05b4e0efd694e35d71b3e74
Myllaume <guillaume.brioudes@myllaume.fr>
2022-10-15 11:03:23 +0200

    fix user data dir make message

--- controllers/user-data-dir.js
+++ controllers/user-data-dir.js
@@ -4,13 +4,13 @@ const Config = require('../models/config-cli');
 
 module.exports = function() {
     if (fs.existsSync(Config.pathConfigDir)) {
-        console.log('Cosma user data directory already exists');
+        console.log('Cosma user data directory already exists at', ['\x1b[2m', Config.pathConfigDir, '\x1b[0m'].join(''));
     } else {
         fs.mkdir(Config.pathConfigDir, (err) => {
             if (err) {
                 console.error(['\x1b[31m', 'Err.', '\x1b[0m'].join(''), 'can not make Cosma user data directory : ' + err);
             }
-            console.log(['\x1b[32m', 'Cosma user data directory was made', '\x1b[0m'].join(''), `${['\x1b[2m', Config.pathConfigDir, '\x1b[0m'].join('')}`);
+            console.log(['\x1b[32m', 'Cosma user data directory was made at', '\x1b[0m'].join(''), ['\x1b[2m', Config.pathConfigDir, '\x1b[0m'].join(''));
         })
     }
 }
