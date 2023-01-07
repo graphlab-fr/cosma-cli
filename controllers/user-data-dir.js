05e7c5892f56009341e6f771a80ec0f15947fbe2
Myllaume <guillaume.brioudes@myllaume.fr>
2023-01-07 12:02:50 +0100

    fix use-data-dir mkdir windows

--- controllers/user-data-dir.js
+++ controllers/user-data-dir.js
@@ -6,7 +6,7 @@ module.exports = function() {
     if (fs.existsSync(Config.pathConfigDir)) {
         console.log('User data directory already exists at', ['\x1b[2m', Config.pathConfigDir, '\x1b[0m'].join(''));
     } else {
-        fs.mkdir(Config.pathConfigDir, (err) => {
+        fs.mkdir(Config.pathConfigDir, { recursive: true }, (err) => {
             if (err) {
                 console.error(['\x1b[31m', 'Err.', '\x1b[0m'].join(''), 'cannot create user data directory: ' + err);
             }
